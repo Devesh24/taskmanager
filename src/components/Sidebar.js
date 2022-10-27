60131b23c48ed849192b77a05c5fa1c69b865a5e
Devesh Raghuvanshi <devesh12200@gmail.com>
2022-10-27 23:56:54 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,7 @@ export default function Sidebar(props) {
 
       <span className='sidebar ps-2 pe-2 mt-3'>
         <div className="my-2">
-          <Link className="link icon py-3" to="/"><i className="fa-regular fa-clipboard fa-xl px-3 py-4" /></Link>
+          <Link className="link icon py-3" to="/taskmanager"><i className="fa-regular fa-clipboard fa-xl px-3 py-4" /></Link>
         </div>
         <div className="my-2">
           <Link className="link icon py-3" to="/remind"><i className="fa-regular fa-bell fa-xl px-3 py-4" /></Link>
@@ -19,7 +19,7 @@ export default function Sidebar(props) {
       </span >
 
       <span id="slider" className='pe-5 pt-3' style={{display: props.slide}}>
-        <Link className='link' to="/"><div className="iconText fs-4 py-2 ps-4 my-2">Tasks</div></Link>
+        <Link className='link' to="/taskmanager"><div className="iconText fs-4 py-2 ps-4 my-2">Tasks</div></Link>
         <Link className='link' to="/remind"><div className="iconText fs-4 py-2 ps-4 pe-5 my-2">Reminders</div></Link>
         <Link className='link' to="/list"><div className="iconText fs-4 py-2 ps-4 pe-5 my-3">Lists</div></Link>
       </span>
